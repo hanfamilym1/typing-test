24753dae5b4ee437d53d0b606c52146359ddf699
Michael han <michael@hanfamily.com>
2021-03-04 12:41:14 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,107 @@
+
+import React, {useState, useEffect} from 'react';
+import ogWords from './ogWords'
+import './App.css';
+
+
+function App() {
+  // let firstRandomNum = Math.floor(Math.random() * ogWords.length)
+  // let firstWord = ogWords[firstRandomNum]
+  const [count, setCount] = useState(()=> 0)
+  const [usedWords, setUsedWords] = useState([])
+  const [word, setWord] = useState('')
+  const [typedWord, setTypedWord] = useState('')
+  const [finished, setFinished] = useState(false)
+  const [initiatedGame, setInitiatedGame] = useState(false)
+  const [timer, setTimer] = useState(60)
+
+  const chooseNewWord = ()=> {
+    // newWords are words filtered with the usedWords in order to take them out
+    let newWords = ogWords.filter(word => !usedWords.includes(word))
+    let randomNum = Math.floor(Math.random() * newWords.length)
+    // chosen word after this;
+    let newWord = newWords[randomNum]
+    if(newWords.length === 0){
+      setFinished(true)
+    }
+    setUsedWords(usedWords => [...usedWords, newWord])
+    wordsPerMin() 
+    return newWord
+  }
+
+  const wordsPerMin = () => {
+    console.log(usedWords)
+    setCount(usedWords.length)
+  }
+
+  // play Game needs to have the logic between the content.
+  var countdown;
+  const playGame = () => {
+    let newWord = chooseNewWord()
+    setWord(newWord)
+    setInitiatedGame(true)
+    setFinished(false)
+    setTimer(60)
+  }
+
+  useEffect(()=> {
+      countdown = setInterval(()=> {
+         console.log(timer)
+         if(timer === 0 ){
+           clearInterval(countdown)
+         } else {
+           let newTimer = timer-1
+           setTimer(newTimer)
+         }
+      },1000)
+  }, [timer])
+
+  const handleChange = (event) => {
+    if(usedWords.length === 0) {
+      setInitiatedGame(true)
+    }
+    setTypedWord(event.target.value)
+    if(typedWord === word){
+      let newWord = chooseNewWord()
+      setWord(newWord)
+      setTypedWord('')
+    } 
+  }
+
+  const tryAgain = () => {
+    setInitiatedGame(false)
+    setTimer(60)
+    setFinished(false)
+  }
+
+
+
+  return (
+    <div className="App">
+      {!initiatedGame ?
+        <>
+          <h1>Start Game</h1>
+          <button onClick={playGame} >Start</button>
+        </>
+        :
+        [
+          finished ? 
+            <>
+            <h1>COMPLETED</h1>
+            <h3>{count} WPM</h3>
+            <button onClick={tryAgain}>Try Again</button>
+            </>
+        :  
+          <>        
+            <h1>{word}</h1>
+            <input type="text" value={typedWord} onChange={handleChange}/>
+            <h3>{count}</h3>
+            <h4>{timer}</h4>
+          </>
+        ]      
+      }
+    </div>
+  );
+}
+
+export default App;
